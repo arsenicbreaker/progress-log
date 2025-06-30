4dd9ef4c3abf683cd7bde145942d074094112944
arsenicbreaker <inyourminddx@gmail.com>
2025-06-30 07:06:53 +0700

    initial commit

--- Leetcode/src/LastProject.java
+++ Leetcode/src/LastProject.java
@@ -4,24 +4,44 @@ class Pendataan {
     String nama;
     String barang;
     int jumlah;
+    double total;
 
     public Pendataan(String nama, String barang, int jumlah) {
-    this.nama = nama;
-    this.barang = barang;
-    this.jumlah = jumlah;
+        this.nama = nama;
+        this.barang = barang;
+        this.jumlah = jumlah;
+        this.total = hitungTotal();
     }
 
     public void tampilkanInfo() {
         System.out.println("PENDATAAN");
         System.out.println("Nama customer : " + nama);
         System.out.println("Nama barang : " + barang);
-        System.out.println("Total harga : ");
-
+        System.out.println("Total harga : " + total);
     }
+
+    public double hitungTotal() {
+        double hargaSatuan;
+
+        if (barang.equalsIgnoreCase("KULKAS")) {
+            hargaSatuan = 2000000;
+        } else if (barang.equalsIgnoreCase("TV")) {
+            hargaSatuan = 1500000;
+        } else if (barang.equalsIgnoreCase("MESIN CUCI")) {
+            hargaSatuan = 1800000;
+        } else {
+            hargaSatuan = 0;
+        }
+
+        return hargaSatuan * jumlah;
+    }
+
 
 }
 
+
 public class LastProject {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("===SISTEM PENDATAAN PENJUALAN TOKO ELEKTRONIK===");
@@ -41,6 +61,7 @@ public class LastProject {
             String barang = scanner.nextLine();
             System.out.print("Jumlah barang : ");
             int jumlah = scanner.nextInt();
+            scanner.nextLine();
 
             // membuat objek
             datanya[i] = new Pendataan(nama,barang,jumlah);
